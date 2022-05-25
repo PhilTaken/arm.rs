07042915736c62d7dae478517c739489cb11a7d4
Philipp Herzog <philipp.herzog@protonmail.com>
2022-05-25 15:43:57 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, naersk }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages."${system}";
+        naersk-lib = naersk.lib."${system}";
+      in
+        rec {
+          # `nix build / nix run`
+          packages.hello-world = naersk-lib.buildPackage {
+            pname = "arm-rs";
+            root = ./.;
+          };
+          defaultPackage = packages.hello-world;
+
+          # `nix develop`
+          devShell = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [ rustc cargo ];
+          };
+        }
+    );
+}
