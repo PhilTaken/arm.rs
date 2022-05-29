c5da4b386d4a2fdaeb4512a1a5591876576df38f
Philipp Herzog <philipp.herzog@protonmail.com>
2022-05-30 00:56:13 +0200

    feat: event handling closure

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,33 @@ mod media;
 mod config;
 mod devices;
 
-//use config::Config;
+use core::time;
+use std::thread;
 
-fn main() -> Result<(), anyhow::Error> {
-    devices::poll(|event| {
-        println!("{:?}", event.event_type());
-    })
+use media::media_from_dev;
+use config::Config;
+
+fn main() {
+    let config = Config::minimal();
+
+    #[allow(unused_variables)]
+    if let Ok(conf) = config {
+        println!("parsed config, starting...");
+
+        devices::poll(|event| {
+            println!("{:?}", event.event_type());
+            if let Some(media) = media_from_dev(&event.device()) {
+
+                //let _ = media.process(&conf);
+
+                println!("------------------------------------------------");
+
+                thread::sleep(time::Duration::from_secs(10));
+
+                media.eject();
+            };
+        });
+    } else {
+        eprintln!("Error in config: {}", config.err().unwrap());
+    }
 }
