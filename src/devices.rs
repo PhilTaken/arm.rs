f93cd57f2d543d34711d6f98260d0c31434659cb
Philipp Herzog <philipp.herzog@protonmail.com>
2022-05-30 00:42:39 +0200

    feat: poll event handling via closure

--- src/devices.rs
+++ src/devices.rs
@@ -1,31 +1,33 @@
 use mio::{Events, Poll, Interest, Token, unix::SourceFd};
 use std::os::unix::io::AsRawFd;
 use udev::Event;
-use anyhow::Error;
 
 const TOKEN_SOCKET: Token = Token(0);
 
-pub fn poll(f: fn(Event)) -> Result<(), Error> {
-    let mut poll = Poll::new()?;
+pub fn poll<P>(f: P)
+where P: Fn(&Event) {
+    let mut poll = Poll::new().unwrap();
     let mut events = Events::with_capacity(120);
 
-    let socket = udev::MonitorBuilder::new()?
-        .match_subsystem("block")?
-        .listen()?;
+    let socket = udev::MonitorBuilder::new().unwrap()
+        .match_subsystem("block").unwrap()
+        .listen().unwrap();
 
-    poll.registry().register(&mut SourceFd(&socket.as_raw_fd()), TOKEN_SOCKET, Interest::READABLE)?;
+    poll.registry()
+        .register(
+            &mut SourceFd(&socket.as_raw_fd()),
+            TOKEN_SOCKET,
+            Interest::READABLE)
+        .unwrap();
 
     loop {
         let poll_timeout = None;
-        poll.poll(&mut events, poll_timeout)?;
+        poll.poll(&mut events, poll_timeout).unwrap();
 
         for event in &events {
             match event.token() {
                 TOKEN_SOCKET => {
-                    socket.clone().for_each(|event| {
-                        f(event);
-                        //println!("{:?}", event.event_type());
-                    });
+                    socket.clone().for_each(|event| f(&event));
                 },
                 _ => unimplemented!("other token responsibilities")
             }
